eb4e7bb4bea10f168f4aa3c6a22942ee71085176
Ishraq Syed <Ishraq.syed0407@gmail.com>
2026-02-05 18:41:32 +0530

    Changes for branch 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+
+  ngOnInit(): void {
+    const result = this.doSubtract(10, 5);
+    console.log('Subtraction result:', result);
+  }
+
+  doSubtract(a: number, b: number): number {
+    return a - b;
+  }
 }
